761541b5da148cf01c567423c10098bf09a19d74
Jakub Konečný <cubase@cubase-mac.local>
2020-08-20 15:24:12 +0200

    Linked resources, fixed typo

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ import 'react-native-gesture-handler'
 import * as React from 'react'
 
 import { NavigationContainer } from '@react-navigation/native'
-import Navigatation from './navigation'
+import Navigation from './navigation'
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Navigatation />
+      <Navigation />
     </NavigationContainer>
   )
 }
